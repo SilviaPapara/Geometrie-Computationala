f161fc6a4e45dfb17205f5af1932a7ea5ed7f6b0
Silvia Papara <56363962+SilviaPapara@users.noreply.github.com>
2020-05-26 12:22:19 +0300

    Add files via upload

---
+++ Seminar numarul 6 - triunghiuri, cercuri si patrate/Seminar numarul 6 - triunghiuri, cercuri si patrate/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Seminar_numarul_6___triunghiuri__cercuri_si_patrate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(916, 520);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button2.Location = new System.Drawing.Point(975, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 82);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Generare triunghiuri";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Chocolate;
+            this.button3.Location = new System.Drawing.Point(975, 193);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 82);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Generare patrate";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Goldenrod;
+            this.button4.Location = new System.Drawing.Point(975, 331);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 82);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Generare cercuri";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 555);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Seminar numarul 6 - triunghiuri, cercuri si patrate/Seminar numarul 6 - triunghiuri, cercuri si patrate/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Seminar_numarul_6___triunghiuri__cercuri_si_patrate
+{
+   
+    public partial class Form1 : Form
+    {
+        static Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        Bitmap bmp;
+        Graphics grp;
+        
+        //int xA, yA, xB, yB, xC, yC;
+
+            //Generare PATRATE
+        private void button3_Click(object sender, EventArgs e)
+        {
+            grp.Clear(BackColor);
+            
+            grp.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            for (int i = 0; i < 8; i++)
+            {
+                int n = 4, x, y;
+                float l = (float)rnd.Next(30, 100);
+                x = rnd.Next((int)l, pictureBox1.Width - (int)l);
+                y = rnd.Next((int)l, pictureBox1.Height - (int)l);
+                float a = (float)rnd.NextDouble() * (float)Math.PI * 2;
+                Color draw = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                FigGeometrica2(n, l, new PointF(x, y), a, draw);
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            grp = Graphics.FromImage(bmp);
+
+            //Culoarea de stergere a ecranului
+            Color backColor = Color.Orchid;
+
+            pictureBox1.Image = bmp;
+
+        }
+
+        public void FigGeometrica2(int n, float l, PointF center, float phi, Color Draw)
+        {
+            Pen pen = new Pen(Color.Blue);
+            float unghi = (float)(Math.PI * 2) / n;
+            PointF[] points = new PointF[n];
+            for (int i = 0; i < n; i++)
+            {
+                float x = center.X + l * (float)Math.Cos(unghi * i + phi);
+                float y = center.Y + l * (float)Math.Sin(unghi * i + phi);
+                points[i] = new PointF(x, y);
+            }
+            grp.DrawPolygon(new Pen(Draw, 2), points);
+        }
+        //generare TRIUNGHIURI
+        private void button2_Click(object sender, EventArgs e)
+        {
+            grp.Clear(BackColor);
+            for (int i = 0; i < 8; i++)
+            {
+
+               
+
+                PointF[] t = new PointF[3];
+                t[0].X = rnd.Next(300);
+                t[0].Y = rnd.Next(300);
+                t[1].X = rnd.Next(300);
+                t[1].Y = rnd.Next(300);
+                t[2].X = rnd.Next(300);
+                t[2].Y = rnd.Next(300);
+                Color RandomColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                Pen p = new Pen(RandomColor, 3);
+                Pen sb = new Pen(RandomColor, 3);
+                //grp.FillPolygon(sb, t);
+                grp.DrawPolygon(p, t);
+                pictureBox1.Image = bmp;
+
+                
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            grp.Clear(BackColor);
+            grp.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            for (int i = 0; i < 8; i++)
+            {
+                int n = 100, x, y;
+                float l = (float)rnd.Next(30, 100);
+                x = rnd.Next((int)l, pictureBox1.Width - (int)l);
+                y = rnd.Next((int)l, pictureBox1.Height - (int)l);
+                float a = (float)rnd.NextDouble() * (float)Math.PI * 2;
+                Color draw = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                FigGeometrica2(n, l, new PointF(x, y), a, draw);
+            }
+            pictureBox1.Image = bmp;
+        }
+    }
+}
