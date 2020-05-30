ecf325b5aec0b44923819a9f81ec3fa18ca0d41d
Silvia Papara <56363962+SilviaPapara@users.noreply.github.com>
2020-05-30 12:57:32 +0300

    Add files via upload

---
+++ Seminar 9 - Bun/Seminar 9 - Bun/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Seminar_9___Bun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Generare = new System.Windows.Forms.Button();
+            this.UpDownPointCount = new System.Windows.Forms.NumericUpDown();
+            this.ScanareGraham = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Introduceti = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpDownPointCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(891, 441);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Generare
+            // 
+            this.Generare.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Generare.Location = new System.Drawing.Point(948, 27);
+            this.Generare.Name = "Generare";
+            this.Generare.Size = new System.Drawing.Size(134, 83);
+            this.Generare.TabIndex = 1;
+            this.Generare.Text = " Algoritmul lui Jarvis";
+            this.Generare.UseVisualStyleBackColor = false;
+            this.Generare.Click += new System.EventHandler(this.Generare_Click);
+            // 
+            // UpDownPointCount
+            // 
+            this.UpDownPointCount.Location = new System.Drawing.Point(952, 179);
+            this.UpDownPointCount.Name = "UpDownPointCount";
+            this.UpDownPointCount.Size = new System.Drawing.Size(130, 22);
+            this.UpDownPointCount.TabIndex = 2;
+            // 
+            // ScanareGraham
+            // 
+            this.ScanareGraham.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ScanareGraham.Location = new System.Drawing.Point(948, 261);
+            this.ScanareGraham.Name = "ScanareGraham";
+            this.ScanareGraham.Size = new System.Drawing.Size(134, 84);
+            this.ScanareGraham.TabIndex = 3;
+            this.ScanareGraham.Text = "Scanare Graham";
+            this.ScanareGraham.UseVisualStyleBackColor = false;
+            this.ScanareGraham.Click += new System.EventHandler(this.ScanareGraham_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(948, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 79);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Quck Hull";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Introduceti
+            // 
+            this.Introduceti.AutoSize = true;
+            this.Introduceti.Location = new System.Drawing.Point(949, 129);
+            this.Introduceti.Name = "Introduceti";
+            this.Introduceti.Size = new System.Drawing.Size(153, 34);
+            this.Introduceti.TabIndex = 5;
+            this.Introduceti.Text = "Introduceti numarul 20 \r\nin casuta de mai jos";
+            this.Introduceti.Click += new System.EventHandler(this.Introduceti_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1172, 515);
+            this.Controls.Add(this.Introduceti);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ScanareGraham);
+            this.Controls.Add(this.UpDownPointCount);
+            this.Controls.Add(this.Generare);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UpDownPointCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Generare;
+        private System.Windows.Forms.NumericUpDown UpDownPointCount;
+        private System.Windows.Forms.Button ScanareGraham;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Introduceti;
+    }
+}
+
---
+++ Seminar 9 - Bun/Seminar 9 - Bun/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seminar_9___Bun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        Graphics grp;
+        Bitmap bmp;
+        List<Point> points;
+        HashSet<Point> CHS;
+        Dictionary<Point, string> pointText;
+        Random rnd;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            grp = Graphics.FromImage(bmp);
+        }
+
+        private void Generare_Click(object sender, EventArgs e) // Buton pt Jarvis
+        {
+            grp.Clear(pictureBox1.BackColor);
+            points = new List<Point>();
+            pointText = new Dictionary<Point, string>();
+
+            int n = (int)UpDownPointCount.Value;
+            if (n < 2)
+            {
+                MessageBox.Show("Va rog sa introduceti numarul de puncte ");
+                return;
+            }
+            points = GetRandomPoints(n);
+            for (int i = 0; i < points.Count; i++)
+                pointText.Add(points[i], "P" + (i + 1));
+
+            DrawPoints(points, Color.Black, true);
+            DrawHull();
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void DrawPoints(List<Point> points, Color PointColor, bool text)
+        {
+            int pointWidth = 3;
+
+
+
+            Font textFont = new Font("Arial", 9, FontStyle.Regular);
+            for (int i = 0; i < points.Count; i++)
+            {
+                grp.DrawEllipse(new Pen(PointColor, pointWidth), points[i].X, points[i].Y, pointWidth, pointWidth);
+                grp.FillEllipse(new SolidBrush(PointColor), points[i].X, points[i].Y, pointWidth, pointWidth);
+                if (text == true)
+                    grp.DrawString("P" + (i + 1), textFont, new SolidBrush(Color.Black), points[i]);
+            }
+
+        }
+
+        List<Point> GetRandomPoints(int n)
+        {
+            rnd = new Random((int)System.DateTime.Now.Ticks);
+            List<Point> returnPoints = new List<Point>();
+            for (int i = 0; i < n; i++)
+            {
+                Point toAdd;
+                do
+                {
+                    int x = rnd.Next() % (pictureBox1.Width - 10);
+                    int y = rnd.Next() % (pictureBox1.Height - 5);
+                    toAdd = new Point(x, y);
+                } while (returnPoints.Contains(toAdd));
+                returnPoints.Add(toAdd);
+            }
+            return returnPoints;
+        }
+
+        #region Quick Hull
+       
+        int findSide(Point p1, Point p2, Point p)
+        {
+            int val = (p.Y - p1.Y)*(p2.X - p1.X) - (p2.Y - p1.Y)*(p.X - p1.X);
+
+            if (val > 0)
+                return 1;
+            if (val < 0)
+                return -1;
+            return 0;
+        }
+        int SquaredDistance(Point p1, Point p2)
+        {
+            return Math.Abs((p2.Y - p1.Y)*(p2.Y - p1.Y) + (p2.X - p1.X)*(p2.X - p1.X));
+        }
+        int LineDist(Point p1, Point p2, Point p)
+        {
+            return Math.Abs((p.Y - p1.Y) * (p2.X - p1.X) -
+                       (p2.Y - p1.Y) * (p.X - p1.X));
+        }
+        void DrawHull()
+        {
+            int minX = 0, maxX = 0;
+            for (int i = 1; i < points.Count; i++)
+            {
+                if (points[i].X > points[maxX].X)
+                    maxX = i;
+                if (points[i].X < points[minX].X)
+                    minX = i;
+            }
+            CHS = new HashSet<Point>();
+            CHS.Add(points[minX]);
+            CHS.Add(points[maxX]);
+            Point aux1 = points[minX], aux2 = points[maxX];
+            points.Remove(aux1);
+            points.Remove(aux2);
+
+            QuickHull(points, aux1, aux2, 1);
+            QuickHull(points, aux1, aux2, -1);
+
+
+
+            List<Point> CHSlist = Jarvis(CHS.ToList());
+
+            for (int i = 0; i < CHSlist.Count - 1; i++)
+            {
+                grp.DrawLine(new Pen(Color.Black, 1), CHSlist[i], CHSlist[i + 1]);
+            }
+            grp.DrawLine(new Pen(Color.Black, 1), CHSlist[0], CHSlist[CHSlist.Count - 1]);
+
+
+            DrawPoints(CHS.ToList(), Color.Orange, false);
+
+
+
+        }
+
+        private void QuickHull(List<Point> points, Point p1, Point p2, int side)
+        {
+            int index = -1;
+            int distMax = 0;
+            for (int i = 0; i < points.Count; i++)
+            {
+                int temp = LineDist(p1, p2, points[i]);
+                if (findSide(p1, p2, points[i]) == side && temp > distMax)
+                {
+                    index = i;
+                    distMax = temp;
+                }
+            }
+            if (index == -1)
+            {
+
+                CHS.Add(p1);
+
+                CHS.Add(p2);
+                return;
+            }
+            Point toRemove = new Point(points[index].X, points[index].Y);
+           
+            QuickHull(points, toRemove, p1, -findSide(toRemove, p1, p2));
+            QuickHull(points, toRemove, p2, -findSide(toRemove, p2, p1));
+
+        }
+        #endregion
+
+        #region Jarvis
+        public void Swap(ref Point a, ref Point b)
+        {
+            Point aux = new Point();
+            aux = a;
+            a = b;
+            b = aux;
+
+
+        }
+        private double Distance(Point A, Point B)
+        {
+            return Math.Sqrt((B.X - A.X)*(B.X - A.X) + (B.Y - A.Y)*(B.Y - A.Y));
+        }
+        private float Orientare(Point A, Point B, Point C)
+        {
+            double temp = (B.Y - A.Y)*(C.X - A.X) - (C.Y - A.Y)*(B.X - A.X);
+            if (temp < 0)
+                return -1;
+            else if (temp == 0)
+                return 0;
+            else
+                return 1;
+        }
+        private List<Point> Jarvis(List<Point> points)
+        {
+
+            //se afla punctul cu  y minim, daca sunt mai multe puncte cu y min, se ia cea cu x minim.
+            int indexmin = 0;
+            for (int i = 1; i < points.Count; i++)
+                if (points[i].Y < points[indexmin].Y || (points[i].Y == points[indexmin].Y && points[i].X > points[indexmin].X))
+                    indexmin = i;
+            List<Point> CHS = new List<Point>();
+            CHS.Add(points[indexmin]);
+            // points.Remove(points[indexmin]);
+            for (int i = 0; i < CHS.Count; i++)
+            {
+                Point nextPoint = (CHS[i] == points[0]) ? points[1] : points[0];
+                for (int j = 0; j < points.Count; j++)
+                {
+                    float det = Orientare(CHS[i], points[j], nextPoint);
+                    if (det > 0 || (det == 0
+                        && Distance(CHS[i], points[j]) > Distance(CHS[i], nextPoint)))
+                        nextPoint = points[j];
+                }
+
+                if (nextPoint != CHS[0])
+                    CHS.Add(nextPoint);
+            }
+            return CHS;
+        }
+
+        #endregion
+
+        private void ScanareGraham_Click(object sender, EventArgs e)
+        {
+            grp.Clear(pictureBox1.BackColor);
+            points = new List<Point>();
+            points.Add(new Point(10 * 3, 110 * 3));
+            points.Add(new Point(20 * 3, 90 * 3));
+            points.Add(new Point(30 * 3, 100 * 3));
+            points.Add(new Point(40 * 3, 90 * 3));
+            points.Add(new Point(50 * 3, 70 * 3));
+            points.Add(new Point(60 * 3, 70 * 3));
+            points.Add(new Point(70 * 3, 110 * 3));
+
+            DrawPoints(points, Color.Black, true);
+            DrawHull();
+            pictureBox1.Image = bmp;
+
+         
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Quick Hull
+        {
+            grp.Clear(pictureBox1.BackColor);
+            points = new List<Point>();
+
+            points.Add(new Point(20*3, 80 * 3));
+            points.Add(new Point(10*3, 90 * 3));
+            points.Add(new Point(50*3, 60 * 3));
+            points.Add(new Point(80*3, 90 * 3));
+            points.Add(new Point(10 * 3, 30 * 3));
+            points.Add(new Point(60 * 3, 70 * 3));
+            points.Add(new Point(60 * 3, 70 * 3));
+            DrawPoints(points, Color.Black, true);
+            //points.Add(new Point(80, 150));
+
+            DrawPoints(points, Color.Black, true);
+            DrawHull();
+            MessageBox.Show("punctele eliminate sunt: P1(20,80), P6(60,70), P7(60,70)");
+            pictureBox1.Image = bmp;
+        }
+
+     
+
+        private void Introduceti_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
